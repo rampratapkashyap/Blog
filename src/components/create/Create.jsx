3f331a73cc119a9a54a87b7628318ada3c11631a
ramkashyap <rampratap.kashyap@razorse.com-at-099391646783>
2024-04-09 12:47:51 +0530

    update
    update

--- src/components/create/Create.jsx
+++ src/components/create/Create.jsx
@@ -1,13 +1,12 @@
 import React from "react"
 import "./create.css"
-import { IoIosAddCircleOutline } from "react-icons/io"
 
 export const Create = () => {
   return (
     <>
       <section className='newPost'>
         <div className='container boxItems'>
-          <div className='img '>
+          <div className='img'>
             <img src='https://images.pexels.com/photos/6424244/pexels-photo-6424244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='image' class='image-preview' />
           </div>
           <form>
