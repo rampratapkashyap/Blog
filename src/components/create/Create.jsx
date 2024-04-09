691cfd7a3526025c5a3202106c99209df1a5e42a
ramkashyap <rampratap.kashyap@razorse.com-at-099391646783>
2024-04-09 13:11:13 +0530

    update
    update

--- src/components/create/Create.jsx
+++ src/components/create/Create.jsx
@@ -7,7 +7,7 @@ export const Create = () => {
       <section className='newPost'>
         <div className='container boxItems'>
           <div className='img'>
-            <img src='https://images.pexels.com/photos/6424244/pexels-photo-6424244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='image' class='image-preview' />
+            {/* <img src='https://images.pexels.com/photos/6424244/pexels-photo-6424244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='image' class='image-preview' /> */}
           </div>
           <form>
             <div className='inputfile flexCenter'>
